afef82c1bc46bd274d3cb1729db89f3cbc2c29fe
one <one@onenoMacBook-Air.local>
2018-04-18 17:20:23 +0900

    second

---
+++ ando/chapter01/knock03.py
@@ -0,0 +1,7 @@
+texnum = []
+w = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+w = re.sub('[,|.]',"",w)
+w = w.split(" ")
+for i in w:
+    texnum.append(len(i))
+print(texnum)
