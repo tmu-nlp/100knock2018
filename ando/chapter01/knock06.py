77a0e38bc8d2ce82d90eccabd70a83c68c512f9a
one <one@onenoMacBook-Air.local>
2018-04-18 18:54:27 +0900

    commit

--- ando/chapter01/knock06.py
+++ ando/chapter01/knock06.py
@@ -1,3 +1,10 @@
+def gram(wlist):
+    backlist = []
+    for i in range(len(wlist)):
+        if i == len(wlist)-1:
+            return backlist
+        backlist.append(wlist[i]+"-"+wlist[i+1])
+
 seki = []
 sa = []
 w1 = "paraparaparadise"
