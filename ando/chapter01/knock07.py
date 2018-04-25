3884270184e44012299f54f41a37cb6f46d9c15b
one <one@onenoMacBook-Air.local>
2018-04-25 17:16:05 +0900

    commit

--- ando/chapter01/knock07.py
+++ ando/chapter01/knock07.py
@@ -1,7 +1,11 @@
+import sys
+
 def bun(x2,y2,z2):
     sentence = "{}時の{}は{}".format(str(x2),str(y2),str(z2))
     return sentence
-x=12
-y="気温"
-z=22.4
+
+args = sys.argv
+x=args[1]
+y=args[2]
+z=args[3]
 print(bun(x,y,z))
