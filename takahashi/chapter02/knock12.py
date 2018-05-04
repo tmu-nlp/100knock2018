a044a5762f143f64bc1b3acabe80c0fcdb5bb671
romth <roomtheater@gmail.com>
2018-05-05 00:02:30 +0900

    fix mislead of problems

--- takahashi/chapter02/knock12.py
+++ takahashi/chapter02/knock12.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 
 line_count = 0
+col1 = []
+col2 = []
+
 with open('../data/hightemp.txt', 'r', encoding="utf-8_sig") as f:
     for line in f:
-        if line_count == 0:
-            with open('col1.txt', 'w', encoding='utf-8') as wf:
-                wf.write(line)
-        elif line_count == 1:
-            with open('col2.txt', 'w', encoding='utf-8') as wf:
-                wf.write(line)
-        line_count += 1
+        line = line.split()
+        col1.append(line[0])
+        col2.append(line[1])
+    with open('col1.txt', 'w', encoding='utf-8') as wf:
+        for item in col1:
+            wf.write(item + '\n')
+    with open('col2.txt', 'w', encoding='utf-8') as wf:
+        for item in col2:
+            wf.write(item + '\n')
