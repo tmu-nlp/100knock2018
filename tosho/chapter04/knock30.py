560c022af56ac444165d67ae2c217dfdbda7d861
Tosho Hirasawa <tosho.hirasawa@gmail.com>
2018-05-21 16:36:58 +0900

    fixup knock30

--- tosho/chapter04/knock30.py
+++ tosho/chapter04/knock30.py
@@ -3,15 +3,13 @@ sys.path.append(os.pardir)
 
 from common.parsers import iterate_mecab_morphs
 
-def parse_mecab(filename='neko.txt.mecab'):
-    return list(iterate_mecab_morphs(filename))
+def iterate_neko(filename='neko.txt.mecab'):
+    return iterate_mecab_morphs(filename)
 
 if __name__ == '__main__':
-    from random import sample
+    from itertools import islice
 
-    data = parse_mecab()
-
-    for item in sample(parse_mecab(),10):
+    for item in islice(iterate_neko(),10):
         line = ''.join(map(lambda m : m.surface, item))
         print(line)
         for m in item:
---
+++ tosho/chapter04/knock31.py
@@ -0,0 +1,5 @@
+import os,sys
+sys.path.append(os.pardir)
+
+from common.parsers import iterate_mecab_morphs
+
